ac922b75f338bd98d77610897dc1843589e7ea3c
Mark Kvetny <m.kvetny@outlook.com>
2018-10-19 01:21:42 +0200

    added more styles

--- src/scenes/LandingPage.js
+++ src/scenes/LandingPage.js
@@ -1,5 +1,42 @@
 import React from 'react'
+import {
+  Container,
+  Jumbotron
+} from 'reactstrap'
+import styled from 'styled-components'
 
-const LandingPage = () => <div>this is the landing page</div>
+const StyledJumbotron = styled(Jumbotron)`
+  && {
+    background: #f9f9f9;
+    padding: 48px 16px;
+  }
+  @media (min-width: 768px) {
+    && {
+      background: #f9f9f9;
+      padding: 96px 16px;
+    }
+  }
+
+`
+
+const StyledContainer = styled(Container)`
+  && {
+    width: 600px;
+    max-width: 90%;
+  }
+`
+
+const LandingPage = () =>
+  <div>
+    <StyledJumbotron className="text-center">
+      <StyledContainer>
+        <h1 className="jumbotron-heading mb-3">Buy cute puppies</h1>
+        <p className="lead text-muted">
+          This is a very good place to do this,
+          and surely buying cute puppies on the internet isn't illegal.
+        </p>
+      </StyledContainer>
+    </StyledJumbotron>
+  </div>
 
 export default LandingPage
--- src/scenes/LoginPage.js
+++ src/scenes/LoginPage.js
@@ -1,19 +1,28 @@
 import React from 'react'
-import { Form, FormGroup, Label, Input, Container } from 'reactstrap'
+import { Form, FormGroup, Label, Input, Container, Button } from 'reactstrap'
 
 const LoginPage = () =>
   <Container>
-    <h2>Welcome Back!</h2>
-    <Form>
-      <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-      </FormGroup>
-    </Form>
+    <div className="row pt-5">
+      <div className="col">
+        <h2 className="text-center">Welcome Back!</h2>
+      </div>
+    </div>
+    <div className="row justify-content-center pt-4">
+      <div className="col col-lg-6">
+        <Form>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input type="email" name="email" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input type="password" name="password" />
+          </FormGroup>
+          <Button color="primary">Submit</Button>
+        </Form>
+      </div>
+    </div>
   </Container>
 
 export default LoginPage
