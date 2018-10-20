b9103e4a4a62ecfa7819fd2f7e756b747965bd17
Mark Kvetny <m.kvetny@outlook.com>
2018-10-21 01:17:59 +0200

    changed images

--- src/scenes/LandingPage/PuppyList.js
+++ src/scenes/LandingPage/PuppyList.js
@@ -6,13 +6,13 @@ const PuppyList = () =>
   <Container>
     <Row>
       <Col xs="12" md="4" className="mb-4">
-        <PuppyCard name="Snuffles" imageUrl="http://www.placepuppy.net/1p/400/250" />
+        <PuppyCard name="Snuffles" imageUrl="http://www.placepuppy.net/8p/400/250" />
       </Col>
       <Col xs="12" md="4" className="mb-4">
-        <PuppyCard name="Doggo" imageUrl="http://www.placepuppy.net/2p/400/250" />
+        <PuppyCard name="Doggo" imageUrl="http://www.placepuppy.net/18p/400/250" />
       </Col>
       <Col xs="12" md="4" className="mb-4">
-        <PuppyCard name="Puppy" imageUrl="http://www.placepuppy.net/3p/400/250" />
+        <PuppyCard name="Puppy" imageUrl="http://www.placepuppy.net/20p/400/250" />
       </Col>
     </Row>
   </Container>
