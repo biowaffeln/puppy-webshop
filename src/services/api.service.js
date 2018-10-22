f171dceae636ffde0e1ff4e6c25fd431ab867e56
Mark Kvetny <m.kvetny@outlook.com>
2018-10-22 23:27:46 +0200

    added comment

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,17 +1,21 @@
 const fakeData = [
-  {id: 0, name: "Snuffles", imageUrl: "http://www.placepuppy.net/8p/400/250"},
-  {id: 1, name: "Doggo", imageUrl: "http://www.placepuppy.net/18p/400/250"},
-  {id: 2, name: "Puppy", imageUrl: "http://www.placepuppy.net/20p/400/250"},
+  { id: 0, name: "Snuffles", imageUrl: "http://www.placepuppy.net/8p/400/250" },
+  { id: 1, name: "Doggo", imageUrl: "http://www.placepuppy.net/18p/400/250" },
+  { id: 2, name: "Puppy", imageUrl: "http://www.placepuppy.net/20p/400/250" },
 ]
 
 const Api = {
   load() {
+    /*
+      Emuliert einen Database Request.
+      Bitte mit einem echten Request ersetzen.
+    */
     return new Promise((resolve, reject) => {
       window.setTimeout(() => {
         resolve(fakeData)
       }, 200)
     })
-  }
+  },
 }
 
 export default Api
