96dfd1d66370b0c879667702e8ac5db5f03120af
mq003 <mq003@hdm-stuttgart.de>
2018-10-28 15:13:46 +0100

    datenbank lokal mit nutzer postgres pw''

--- Backend/poll/models.py
+++ Backend/poll/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Puppy class rasse und name.
+class Puppy(models.Model):
+    rasse = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    alter = models.IntegerField(2)
+    gewicht = models.IntegerField(2)
+
+
+#class Order(models.Model):
+ #   id = models.IntegerField(5)
+  #  totalPrice = models.DecimalField(max_digits=100)
+   # date = models.DateTimeField(4)
+    #UserId = models.IntegerField(8)
+
+
