8585aace39b1e08e986278210462db79195eb726
Niggls <niggls@hotmail.de>
2018-11-05 16:30:21 +0100

    renamed environment variable so that react can access it

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,14 +1,14 @@
-const BACKEND_URL = process.env.BACKEND_URL
-  ? process.env.BACKEND_URL
+const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+  ? process.env.REACT_APP_BACKEND_URL
   : 'http://localhost:8000'
 
 export default {
   async getAllPuppies() {
-    const res = await fetch(BACKEND_URL + '/shop/puppies/all')
+    const res = await fetch(REACT_APP_BACKEND_URL + '/shop/puppies/all')
     return res.json()
   },
   async getPuppyById(id) {
-    const res = await fetch(BACKEND_URL + `/shop/puppies/${id}`)
+    const res = await fetch(REACT_APP_BACKEND_URL + `/shop/puppies/${id}`)
     return res.json()
   }
 }
