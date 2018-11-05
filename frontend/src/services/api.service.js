816e0523bdf5d70b6bd8325464bc6dece3961e8b
Mark Kvetny <m.kvetny@outlook.com>
2018-11-05 15:36:50 +0100

    added dynamic backend url

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,12 +1,14 @@
-const API_URL = 'http://localhost:8000/shop'
+const BACKEND_URL = process.env.BACKEND_URL
+  ? process.env.BACKEND_URL
+  : 'http://localhost:8000'
 
 export default {
   async getAllPuppies() {
-    const res = await fetch(API_URL + '/puppies/all')
+    const res = await fetch(BACKEND_URL + '/shop/puppies/all')
     return res.json()
   },
   async getPuppyById(id) {
-    const res = await fetch(API_URL + `/puppies/${id}`)
+    const res = await fetch(BACKEND_URL + `/shop/puppies/${id}`)
     return res.json()
   }
 }
