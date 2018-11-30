9d3dedc0563aed278a3c4442443421e229180793
Mark Kvetny <m.kvetny@outlook.com>
2018-11-30 17:48:54 +0100

    added store persistance

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,23 @@ import { createBrowserHistory } from 'history'
 import createReducer from './reducers'
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 export const history = createBrowserHistory()
 
+const persistConfig = {
+  key: 'redux',
+  storage,
+  whitelist: ['language', 'auth']
+}
+
 export const store = createStore(
-  createReducer(history),
+  persistReducer(persistConfig, createReducer(history)),
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ),
 )
+
+export const persistor = persistStore(store)
