7558c1f754c325f5fce782c46c33c2b7bb67fc05
Mark Kvetny <biowaffeln@gmail.com>
2019-01-15 14:46:47 +0100

    error handling on order

--- frontend/src/scenes/OrderListPage/index.js
+++ frontend/src/scenes/OrderListPage/index.js
@@ -6,15 +6,25 @@ import Api from '../../services/api.service'
 class OrderListContainer extends Component {
 
   state = {
-    orders: []
+    orders: [],
+    error: false
   }
 
   async componentDidMount() {
+    try {
+
     const orders = await Api.getMyOrders(this.props.token)
     this.setState({ orders })
+    }
+    catch {
+      this.setState({error: true})
+    }
   }
 
   render() {
+    if(this.state.error) {
+      return <p>ein Fehler ist aufgetreten!</p>
+    }
     return (
       <OrderListPage orders={this.state.orders} language={this.props.language} />
     )
