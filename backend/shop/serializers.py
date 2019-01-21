54d58864da18e10fcfa14953b891f464b159f407
Niggls <niggls@hotmail.de>
2019-01-21 16:25:55 +0100

    User now contains its address and added new address API, removed unused code

--- backend/shop/serializers.py
+++ backend/shop/serializers.py
@@ -1,41 +1,22 @@
 from rest_framework import serializers
-from rest_framework_jwt.settings import api_settings
 from django.contrib.auth.models import User
-from .models import Order, Puppy, PuppyOrder
+from .models import Order, Puppy, PuppyOrder, Address
+
+
+class AddressSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Address
+        fields = ('country', 'street', 'zip', 'city')
 
 
 class UserSerializer(serializers.ModelSerializer):
-    orders = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    address = AddressSerializer()
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'orders')
-
-
-class UserSerializerWithToken(serializers.ModelSerializer):
-
-    token = serializers.SerializerMethodField()
-    password = serializers.CharField(write_only=True)
-
-    def get_token(self, obj):
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-
-        payload = jwt_payload_handler(obj)
-        token = jwt_encode_handler(payload)
-        return token
-
-    def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
-
-    class Meta:
-        model = User
-        fields = ('token', 'username', 'password')
+        read_only_fields = ('orders',)
+        fields = ('id', 'username', 'orders', 'address')
 
 
 class PuppySerializer(serializers.ModelSerializer):
