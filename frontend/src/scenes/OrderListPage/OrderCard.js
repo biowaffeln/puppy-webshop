b77db22edc769835dc57304d8d8292add275f0a6
Alex <alex7339@hotmail.de>
2019-01-21 18:34:05 +0100

    Hässlich und nicht fertig

--- frontend/src/scenes/OrderListPage/OrderCard.js
+++ frontend/src/scenes/OrderListPage/OrderCard.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import {
+    Col,
+    Row,
   Card
 } from 'reactstrap'
 
-const OrderCard = ({ order }) => {
+const OrderCard = ({ order, puppy }) => {
   return (
     <div>
       <Card className="shadow-blurred">
-        {JSON.stringify(order, null, 4)}
+        <Row>
+          <Col xs="auto">
+              <h5> ID:  {order.id} </h5>
+          </Col>
+          <Col xs="auto">
+              <h5> Menge:  {order.amount} </h5>
+          </Col>
+            <Col>
+             <h5> Preis:  {order.price}</h5>
+            </Col>
+        </Row>
+
+          {JSON.stringify(order, null, 4)}
       </Card>
     </div>
   )
