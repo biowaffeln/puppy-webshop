40c547c72b4041c176eca8a6df9821ed4c25f4b6
Mark Kvetny <biowaffeln@gmail.com>
2019-01-21 19:35:50 +0100

    installed some stuff + fixed hostref error

--- frontend/src/components/Logo/Basket.js
+++ frontend/src/components/Logo/Basket.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Basket = props => (
     <svg viewBox='0 0 234 185' xmlns='http://www.w3.org/2000/svg' fillRule='evenodd'
-         clipRule='evenodd' strokeLinejoin='round' strokeMiterlimit='1.414'>
+        clipRule='evenodd' strokeLinejoin='round' strokeMiterlimit='1.414'
+        className={props.className}
+        ref={props.hostRef}
+    >
         <g id='Layer_1-2' fill='#fff' fillRule='nonzero' stroke='#000'>
             <path d='M56.27,94.03L8.58,87.17L0.68,92.18L14.38,157.52L11.74,161.73L12.01,166.47L31.24,174.38L66.02,181.49L106.59,184.39L161.25,181.49L199.06,174.38L220.4,167L220.4,163.05L218.55,159.1L233.04,92.71L222.77,87.17L175.61,93.5L173.77,112.99L167.44,133.02L154.53,148.3L131.35,159.36L106.59,160.68L82.35,153.56L68.91,140.92L60.22,125.85L56.01,104.83L56.27,94.03Z'
-                  strokeWidth='1.1' />
+                strokeWidth='1.1' />
             <path d='M0.68,92.18L25.71,16.04L65.72,0.5L173.24,0.5L211.97,19.73L233.04,92.71L222.77,92.71L202.48,22.63L170.61,6.82L69.18,6.82L35.98,18.94L11.48,92.71L0.68,92.18Z'
-                  strokeLinejoin='miter' strokeMiterlimit='10' />
+                strokeLinejoin='miter' strokeMiterlimit='10' />
         </g>
     </svg>)
 export default Basket
